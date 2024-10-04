0bcd5008744b04f8bab1233e7e2abb0f64f7a01f
Max <51026114+maxKostyukovich@users.noreply.github.com>
2024-10-04 10:39:27 +0200

    Update hash-password.pipe.ts

--- src/shared/pipes/hash-password.pipe.ts
+++ src/shared/pipes/hash-password.pipe.ts
@@ -7,7 +7,6 @@ import { AddUserRequestDto } from '../../modules/user/dto/add-user-request.dto';
 export class HashPasswordPipe implements PipeTransform {
   async transform(value: AddUserRequestDto, metadata: ArgumentMetadata) {
     if (value.password) {
-      console.log(process.env.SALT + ' salt');
       const salt = await bcrypt.genSalt(+process.env.SALT || 10);
       value.password = await bcrypt.hash(value.password, salt);
     }
